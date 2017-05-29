a5d20df183eceaedf8a808b3d6a2fa2e992de10a
Thanh Mi Nguyen Thanh <thanhmi1111@live.com>
2017-05-29 22:38:53 +0700

    first commit

--- nth_prime.go
+++ nth_prime.go
@@ -1,3 +1,33 @@
 package prime
 
+import (
+	"math"
+)
+
 const testVersion = 1
+
+func Nth(nth int) (int, bool) {
+	if nth == 1 {
+		return 2, true
+	} else if nth == 0 {
+		return 0, false
+	}
+	n := float64(float64(nth)*math.Log(float64(nth)) + float64(nth)*math.Log(math.Log(float64(nth))) + 6)
+	a := make([]bool, int(n))
+
+	for i := 2; i < int(math.Sqrt(n)); i++ {
+		if !a[int(i)] {
+			for j := i * i; j < int(n); j = j + i {
+				a[j] = true
+			}
+		}
+	}
+	var p int
+	for i := 2; i < int(n) && nth > 0; i++ {
+		if !a[i] {
+			p = i
+			nth--
+		}
+	}
+	return p, true
+}
